bdc52f09f98ca5dc8d714aab5cdbc582d4fac3cd
Maxim Shapovalov <syndex@list.ru>
2024-09-24 10:58:37 +0300

    Init

---
+++ src/main/java/com/github/syndexmx/devtirostar/DevtirostarApplication.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.devtirostar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtirostarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtirostarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/syndexmx/devtirostar/DevtirostarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.devtirostar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtirostarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
