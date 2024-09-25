7192a6299084f2f86475f986cadf8866540573a2
Maxim Shapovalov <syndex@list.ru>
2024-09-25 19:45:01 +0300

    Another matcher in Integration tests

--- src/test/java/com/github/syndexmx/devtirostar/controllers/StarControllerIT.java
+++ src/test/java/com/github/syndexmx/devtirostar/controllers/StarControllerIT.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.syndexmx.devtirostar.domain.Star;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -16,6 +18,7 @@ import static com.github.syndexmx.devtirostar.TestData.testStarEntity;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
 public class StarControllerIT {
 
     @Autowired
@@ -30,5 +33,19 @@ public class StarControllerIT {
                     .contentType("application/json")
                     .content(starJson))
                 .andExpect(MockMvcResultMatchers.content().json(starJson));
+    }
+
+    @Test
+    public void testThatStarIsCreatedJsonPathMatcher() throws Exception {
+        final Star star = testStar();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final String starJson = objectMapper.writeValueAsString(star);
+        mockMvc.perform(MockMvcRequestBuilders.put("/stars/" + star.getDesignator())
+                        .contentType("application/json")
+                        .content(starJson))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.designator").value(star.getDesignator()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(star.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.constellation").value(star.getConstellation()));
     }
 }
